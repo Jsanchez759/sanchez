62781de0c86b82d54f082541fb0b090a3cdb9fee
Jesus Eduardo Sánchez Daza <91790625+Jsanchez759@users.noreply.github.com>
2023-11-10 11:32:38 -0500

    Update Study_material.py

--- JobsDriver_ChatBot/app_quiz/pages/Study_material.py
+++ JobsDriver_ChatBot/app_quiz/pages/Study_material.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Study Material for the quiz :books:')
 
-file_txt = "pages/study_material.txt"
+file_txt = "JobsDriver_ChatBot/app_quiz/pages/study_material.txt"
 with open(file_txt, "r", encoding="utf-8") as file:
         material_study = file.read()
         st.markdown(material_study)
